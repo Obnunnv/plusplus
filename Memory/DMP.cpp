a5ddcd3c5ac2caf4d8ddcc7d09bfa7b446d4c8ff
Obnunnv <obsinungk@gmail.com>
2025-01-15 13:23:42 -0800

    Update DMP.cpp

--- Memory/DMP.cpp
+++ Memory/DMP.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <cstddef>
+#include <cstring>
 
 struct Freeblock {
 	size_t size;
@@ -10,48 +11,74 @@ struct Freeblock {
 
 
 Memorypool::Memorypool(size_t size) {
-    pool = ::operator new(size);
+    pool = malloc(size);
     if (!pool) {
         throw std::bad_alloc();
     }
-
+    freeList = nullptr;
     poolSize = size;
     std::cout << poolSize << " bytes created" << std::endl;
 }
 
 Memorypool::~Memorypool() {
-    ::operator delete(pool);
+    free(pool);
     std::cout << poolSize << " bytes destroyed" << std::endl;
 }
 
 void* Memorypool::allocate(size_t size) {
     Freeblock* prev = nullptr;
-	Freeblock* current = freeList;
-	
-	while (current != nullptr && current->size < size) {
-		prev = current;
-		current = current->next;
-	}
-	if (current -- nullptr) {
-		return ::operator new(size);
-	}
-	
-	else {
-		if (prev) {
-			prev->next = current->next;
-		}
-		else {
-			freeList = current->next;
-		}
-		return reinterpret_cast<void*>(current);
-	}
-	
-	return ::operator new(size);
+    Freeblock* current = freeList;
+
+    while (current != nullptr && current->size < size) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current != nullptr) {
+        if (prev) {
+            prev->next = current->next;
+        } else {
+            freeList = current->next;
+        }
+
+        return reinterpret_cast<char*>(current) + sizeof(Freeblock);
+    }
+
+    void* rawBlock = malloc(size + sizeof(size_t));
+    if (rawBlock) {
+        *((size_t*)rawBlock) = size;
+        return (char*)rawBlock + sizeof(size_t);
+    }
+
+    return nullptr;
+}
+
+size_t Memorypool::getBlocksize(void* ptr) {
+    return *((size_t*)((char*)ptr - sizeof(size_t)));
 }
 
 void Memorypool::release(void* ptr) {
-    F
-	::operator delete(ptr);
+    Freeblock* block = reinterpret_cast<Freeblock*>(ptr);
+	block->next = freeList;
+    freeList = block;
+}
+
+void* Memorypool::reallocate(void* ptr, size_t newSize) {
+    if (ptr == nullptr) {
+        return allocate(newSize);
+
+    }
+    size_t CurrentSize = getBlocksize(ptr);
+
+    if (newSize <= CurrentSize) {
+        return ptr;
+    }
+
+    
+    void* newptr = allocate(newSize);
+    memcpy(newptr, ptr, CurrentSize);
+    release(ptr);
+    return newptr;
 }
 
 int main() {
