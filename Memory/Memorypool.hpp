3d7ad639d78a4f7f0255a889b222a68b6a913d5a
Obnunnv <obsinungk@gmail.com>
2025-01-16 07:36:14 -0800

    Update Memorypool.hpp

--- Memory/Memorypool.hpp
+++ Memory/Memorypool.hpp
@@ -1,5 +1,7 @@
 #include <cstddef>
 #include <cstdlib>
+#include <thread>
+#include <mutex>
 
 #pragma once
 
@@ -8,6 +10,7 @@ private:
     void* pool;
     size_t poolSize;
     struct Freeblock* freeList;
+	std::mutex mtx;
 
 public:
     Memorypool(size_t size);
@@ -17,4 +20,5 @@ public:
     void release(void* ptr);
     void* reallocate(void* ptr, size_t newSize);
     size_t getBlocksize(void* ptr);
+	void MergeAdjacentBlocks();
 };
